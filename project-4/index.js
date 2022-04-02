6bf2c13dd6b390f5bed59cbcdc8dfacaf2602f8c
ibktommy <atomquartz@gmail.com>
2022-04-02 18:29:17 +0100

    Upload of Project four Completion

--- project-4/index.js
+++ project-4/index.js
@@ -10,20 +10,59 @@ const secResult = document.querySelector(".sec-result");
 const result = document.querySelector(".result");
 
 // Calculate Semi-Perimeter
+function setVariables() {
+	let a = parseFloat(num1.value);
+	let b = parseFloat(num2.value);
+	let c = parseFloat(num3.value);
+
+	s = (a + b + c) / 2;
+}
+
+function areaOfTriangle() {
+	let a = parseFloat(num1.value);
+	let b = parseFloat(num2.value);
+	let c = parseFloat(num3.value);
+	s = parseFloat((a + b + c) / 2).toFixed(2);
+}
 
 secBtn.addEventListener("click", () => {
 	if (num1.value === "" || num2.value === "" || num3.value === "") {
-		secResult.textContent = "INPUT ALL FIELDS";
+		secResult.textContent = "PLEASE INPUT ALL FIELDS";
 		setTimeout(() => {
-			secResult.textContent = null;
+			secResult.textContent = "";
 		}, 1500);
+	} else if (num1.value != (num2.value || num3.value)) {
+		secResult.textContent = "The value of the Triangle sides must be same!";
+		setTimeout(() => {
+			secResult.textContent = "";
+		}, 2000);
 	} else {
+		areaOfTriangle();
+		secResult.textContent = `The semi-perimeter of the Triangle is ${s}`;
+	}
+});
+
+// Calculate the Area of the Triangle
+pryBtn.addEventListener("click", () => {
+	if (secResult.textContent === "") {
+		result.textContent = 'PLEASE CALCULATE THE "S" VALUE FIRST';
+		timeout(result.textContent);
+	} else {
+		console.log(s);
 		let a = parseFloat(num1.value);
 		let b = parseFloat(num2.value);
 		let c = parseFloat(num3.value);
 
-		const semiPerimeter = (a + b + c) / 2;
-
-		secResult.textContent = `${semiPerimeter} is the Semi-Perimeter of the Triangle`;
+		let areaVal = s * (s - a) * (s - b) * (s - c);
+		console.log(areaVal);
+		const areaOfTriangle = Math.sqrt(areaVal).toFixed(2);
+		result.textContent = `The Area of the Triangle = ${areaOfTriangle}`;
 	}
+});
+
+// Clear Input Fields
+resetBtn.addEventListener("click", () => {
+	num1.value = "";
+	num2.value = "";
+	num3.value = "";
 });
